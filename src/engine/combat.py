cdc56eb05c37caf098ca7e83a794a90bc67de9ab
DarkDBx <128897403+DarkDBx@users.noreply.github.com>
2023-08-29 11:55:46 +0200

    Fix timing

--- src/engine/combat.py
+++ src/engine/combat.py
@@ -76,66 +76,66 @@ def combat_rotation(value):
                 timer12.StartTimer(4)
                 input_helper.press(skillUlt)
                 info('Execute ability ultimate')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif timer11.GetTimerState() == TIMER_STOPPED:
                 timer11.StartTimer(9)
                 input_helper.press(swap)
                 info('Execute ability weapon swap')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\10.png', conf=0.6) and timer1.GetTimerState() == TIMER_STOPPED:
                 timer1.StartTimer(11)
                 input_helper.press(skill10)
                 info('Execute ability 10')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\09.png', conf=0.6) and not image_helper.pixel_matches_color(940,105, 129,37,37):
                 input_helper.press(skill9)
                 info('Execute ability 9')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\08.png', conf=0.6) and timer3.GetTimerState() == TIMER_STOPPED:
                 timer3.StartTimer(19)
                 input_helper.press(skill8)
                 info('Execute ability 8')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\07.png', conf=0.6) and timer4.GetTimerState() == TIMER_STOPPED:
                 timer4.StartTimer(3)
                 input_helper.press(skill7)
                 info('Execute ability 7')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\06.png', conf=0.6) and timer5.GetTimerState() == TIMER_STOPPED:
                 timer5.StartTimer(3)
                 input_helper.press(skill6)
                 info('Execute ability 6')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\05.png', conf=0.6) and timer6.GetTimerState() == TIMER_STOPPED:
                 timer6.StartTimer(4)
                 input_helper.press(skill5)
                 info('Execute ability 5')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\04.png', conf=0.6) and timer7.GetTimerState() == TIMER_STOPPED:
                 timer7.StartTimer(16)
                 input_helper.press(skill4)
                 info('Execute ability 4')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\03.png', conf=0.6) and timer8.GetTimerState() == TIMER_STOPPED:
                 timer8.StartTimer(21)
                 input_helper.press(skill3)
                 info('Execute ability 3')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\02.png', conf=0.6) and timer9.GetTimerState() == TIMER_STOPPED:
                 timer9.StartTimer(19)
                 input_helper.press(skill2)
                 info('Execute ability 2')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
             elif image_helper.locate_needle(SKILLPATH+value+'\\01.png', conf=0.6) and timer10.GetTimerState() == TIMER_STOPPED:
                 timer10.StartTimer(14)
                 input_helper.press(skill1)
                 info('Execute ability 1')
-                sleep(uniform(0.11, 0.13))
+                sleep(uniform(0.11, 0.14))
         elif value == 'nb_enrage':
             # https://alcasthq.com/eso-stamina-nightblade-build-for-pve/
             pass
 
         input_helper.leftClick()
         info('Execute ability left mouse')
-        sleep(uniform(0.11, 0.13))
+        sleep(uniform(0.11, 0.14))
 
